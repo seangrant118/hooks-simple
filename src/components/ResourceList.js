8a871126bf8e365619f9de844afe6aee72ffaa09
Sean Grant <seangrant118@gmail.com>
2020-03-06 10:32:39 -0500

    refactoring class to a function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,31 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = {
-    resources: []
-  };
-  async componentDidMount() {
+const ResourceList = () => {
+  const [resources, setResources] = useState([]);
+
+  const fetchRespource = async () => {
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/${this.props.resource}`
     );
     this.setState({
       resources: response.data
     });
-  }
-  async componentDidUpdate(prevProps) {
-    if (prevProps !== this.props.resource) {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState({
-        resources: response.data
-      });
-    }
-  }
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  };
+
+  return <div>{resources.length}</div>;
+};
 
 export default ResourceList;
